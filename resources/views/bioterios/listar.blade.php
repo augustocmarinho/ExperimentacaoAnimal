1ef1528cf65ef1e7342fb5cb3dc485acc8325c4f
Kevin Oliveira <kevinols08@gmail.com>
2019-07-10 16:45:20 -0300

    confirmação delete

--- resources/views/bioterios/listar.blade.php
+++ resources/views/bioterios/listar.blade.php
@@ -27,15 +27,40 @@
                                 <th scope="row"><?=$key->id?></th>
                                 <td><?=$key->nome?></td>
                                 <td><?=$key->local?></td>
-                                <td><a href="/bioterios/edit?id=<?=$key->id?>"><i class="fas fa-edit"></i></a> <a href="/bioterios/delete?id=<?=$key->id?>"><i class="fas fa-trash"></i></a></td>
+                                <td><a href="/bioterios/edit?id=<?=$key->id?>"><i class="fas fa-edit"></i></a> <a href="#" onclick="confirmDelete('<?=$key->id?>');" data-toggle="modal" data-target="#Modal"><i class="fas fa-trash"></i></a>
+                                </td>
                             </tr>
                         <?php } ?>
                     </tbody>
                 </table>
             </div>
         </div>
+    </div> 
+</div> 
+</div> 
+
+<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="Modal">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      </div>
+      <div class="modal-body">
+            <span id="text-delete">Tem certeza que deseja apagar o registro?</span>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" id="modal-btn-no" data-dismiss="modal">Cancelar</button>
+        <a type="button" href="#" class="btn btn-danger" id="modal-btn-confirm">Deletar</a>
+      </div>
     </div>
+  </div>
 </div>
-</div>
-</div>
+
+<script>
+    function confirmDelete(id){
+        document.getElementById("modal-btn-confirm").href="/bioterios/delete?id="+id; 
+    }
+</script>
+
 @endsection
